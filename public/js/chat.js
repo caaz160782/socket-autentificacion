83fb7a8a570c851210e9490e61abf869484ab2d5
Toño aguirre <caaz160782@gmail.com>
2023-02-18 14:19:11 -0600

    valida token front

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,36 @@
-const socket=io();
+let usuario=null;
+let socket=null;
+
+
+const validarJWT=async()=>{
+    const token =localStorage.getItem('token') || '';
+
+    if(token.length <= 10){
+        window.location='index.html';
+        throw new Error('No hay token en el servidor');
+    }
+
+    const resp= await  fetch('http://localhost:8000/api/auth',
+     {
+         headers:{
+                    'x-token':token
+                 }        
+     })
+     
+     const {usuario:userDB, token:tokenDB}=await resp.json();
+     //console.log(userDB,tokenDB)
+     localStorage.setItem('token',tokenDB);
+     usuario =userDB;
+}
+
+
+
+const main = async()=>{
+
+    await validarJWT();
+}
+
+main();
+
+//const socket=io();
+
